ad69830614c5a043424fa0b943ba7ca3f160e7f2
bribrown <bribrown@yahoo.com>
2017-03-30 10:45:59 -0400

    Add files via upload

---
+++ ReverseWordOrder.py
@@ -0,0 +1,20 @@
+# Write a program (using functions!) that asks the user for a long string containing multiple words.
+# Print back to the user the same string, except with the words in backwards order.
+
+def main():
+
+    def string_to_reverse():
+        test_string = input('Please input a sentence: ')
+        split_string = test_string.split(' ')
+        split_string_index = len(split_string)- 1
+        rebuilt_string =[]
+
+        while split_string_index >= 0:
+            rebuilt_string.append(split_string[split_string_index])
+            split_string_index -= 1
+
+        print(rebuilt_string)
+
+    string_to_reverse()
+
+if __name__ == "__main__": main()
