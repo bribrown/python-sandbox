d596f4aaf0bf6e4194bc0d0e0600a9699da4ef43
bribrown <bribrown@yahoo.com>
2017-04-14 10:17:39 -0400

    Add files via upload

---
+++ PickWord.py
@@ -0,0 +1,19 @@
+#In this exercise, the task is to write a function that picks a random word from the file [sowpods.txt].
+
+import random
+
+def main():
+    wl = []
+    with open('sowpods.txt', 'r') as open_file:
+        all_text = open_file.readline()
+        while all_text:
+            wl.append(all_text)
+            all_text = open_file.readline()
+
+    wlen = len(wl)-1
+    random_word_index = random.randint(0,wlen)
+    random_word = wl[random_word_index]
+
+    print(random_word)
+
+if __name__ == "__main__": main()
