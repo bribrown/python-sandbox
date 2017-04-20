59519f013594cd58065a42ca59a8c1ca015c9b71
bribrown <bribrown@yahoo.com>
2017-04-20 13:30:07 -0400

    Add files via upload

--- GuessLetters.py
+++ GuessLetters.py
@@ -31,14 +31,17 @@ def main():
     guess_internal = '%' * wordlen
     guess_external = '_ ' * wordlen
     guess_list = []
-    number_of_guesses = 0
+    number_of_incorrect_guesses = 0
+    play_again=''
 
     print()
     print('Welcome to Hangman')
+    print('You are allowed 6 incorrect guesses.')
+    print()
     print('Word: ' + guess_external)
     print()
 
-    while guess_internal != word:
+    while guess_internal != word and number_of_incorrect_guesses <= 6:
         guess = input('Guess a letter, the word, or exit: ')
         print()
         guess = guess.upper()
@@ -57,7 +60,6 @@ def main():
             print()
             continue
         guess_list.append(guess)
-        number_of_guesses += 1
         if guess in word:
             print('Letter ' + guess + ' is in the word!')
             print()
@@ -74,17 +76,113 @@ def main():
             print(guess_external)
             print()
             print('Your guess list: ' + str(guess_list))
-            print('Number of guesses: ' + str(number_of_guesses))
+            print('Number of incorrect guesses: ' + str(number_of_incorrect_guesses))
             print()
         else:
+            number_of_incorrect_guesses += 1
             print('Letter ' + guess + ' is not in the word, please guess again')
             print()
             print(guess_external)
             print()
             print('Your guess list: ' + str(guess_list))
-            print('Number of guesses: ' + str(number_of_guesses))
+            print('Number of incorrect guesses: ' + str(number_of_incorrect_guesses))
             print()
 
-    print('You guessed the word!')
+    print('You are out of incorrect guesses')
+    print()
+    while play_again != 'Y':
+        play_again = input('Would you like to play again (y/n)?')
+        play_again = play_again.upper()
+        if play_again == 'N':
+            exit()
+        elif play_again == 'Y':
+            main()
+        else:
+            print('Please enter \'y\' or \'n\'. Would you like to play again (y/n)?')
+            print()
+            continue
 
 if __name__ == "__main__": main()
+
+# """
+# Practice Python exercise 32
+# Hangman
+# http://www.practicepython.org/exercise/2017/01/10/32-hangman.html
+# """
+#
+# import random
+#
+# def pick_random_word():
+# 	"""
+# 	This function picks a random word from the SOWPODS
+# 	dictionary. 
+# 	"""
+# 	# open the sowpods dictionary
+# 	with open("sowpods.txt", 'r') as f:
+# 		words = f.readlines()
+#
+# 	# generate a random index
+# 	# -1 because len(words) is not a valid index into the list `words`
+# 	index = random.randint(0, len(words) - 1)
+#
+# 	# print out the word at that index
+# 	word = words[index].strip()
+# 	return word
+#
+#
+# def ask_user_for_next_letter():
+# 	letter = input("Guess your letter: ")
+# 	return letter.strip().upper()
+#
+#
+# def generate_word_string(word, letters_guessed):
+# 	output = []
+# 	for letter in word:
+# 		if letter in letters_guessed:
+# 			output.append(letter.upper())
+# 		else:
+# 			output.append("_")
+# 	return " ".join(output)
+#
+#
+# if __name__ == '__main__':
+# 	WORD = pick_random_word()
+#
+# 	letters_to_guess = set(WORD)
+# 	correct_letters_guessed = set()
+# 	incorrect_letters_guessed = set()
+# 	num_guesses = 0
+#
+# 	print("Welcome to Hangman!")
+# 	while (len(letters_to_guess) > 0) and num_guesses < 6:
+# 		guess = ask_user_for_next_letter()
+#
+# 		# check if we already guessed that
+# 		# letter
+# 		if guess in correct_letters_guessed or \
+# 				guess in incorrect_letters_guessed:
+# 			# print out a message
+# 			print("You already guessed that letter.")
+# 			continue
+#
+# 		# if the guess was correct
+# 		if guess in letters_to_guess:
+# 			# update the letters_to_guess
+# 			letters_to_guess.remove(guess)
+# 			# update the correct letters guessed
+# 			correct_letters_guessed.add(guess)
+# 		else:
+# 			incorrect_letters_guessed.add(guess)
+# 			# only update the number of guesses
+# 			# if you guess incorrectly
+# 			num_guesses += 1
+#
+# 		word_string = generate_word_string(WORD, correct_letters_guessed)
+# 		print(word_string)
+# 		print("You have {} guesses left".format(6 - num_guesses))
+#
+# 	# tell the user they have won or lost
+# 	if num_guesses < 6:
+# 		print("Congratulations! You correctly guessed the word {}".format(WORD))
+# 	else:
+# 		print("Sorry, you list! Your word was {}".format(WORD))
