88aaa9c64e7c1718e9aeff6fe656866856e8795e
bribrown <bribrown@yahoo.com>
2017-08-08 13:12:22 -0400

    Add files via upload

--- MadLibs.py
+++ MadLibs.py
@@ -1,11 +1,27 @@
 import re
+import os
+import random
+
+def createMadLibList():
+    madLibList = []
+    os.chdir('C:\\Users\\bbrownholtz\\Desktop')
+    with open('15_Sentences.txt', 'r') as open_file:
+        all_text = open_file.readline()
+        while all_text:
+            all_text = all_text.strip()
+            madLibList.append(all_text)
+            all_text = open_file.readline()
+    return madLibList
 
 def main():
 
+    getMadLibList = createMadLibList()
+    random.shuffle(getMadLibList)
+
+    ml = getMadLibList[0]
+
     pattern = re.compile(r'\.| ')
 
-    ml = 'The ADJECTIVE panda walked to the NOUN and then VERB. A nearby NOUN was unaffected by these events.'
-   # listMl = ml.split()
     reListML = pattern.split(ml)
 
     for i in reListML:
@@ -14,9 +30,6 @@ def main():
             reListML[z] = '.'
         else:
             continue
-
-    print(reListML)
-    print()
 
     for i in reListML:
         if i == ('ADJECTIVE'):
@@ -37,6 +50,11 @@ def main():
     print()
 
     print(newList,end=' ')
+
+    os.chdir('C:\\Users\\bbrownholtz\\Desktop')
+    madLibsFile = open('madLib' + '.txt', 'a')
+    madLibsFile.write(newList + '\n')
+    madLibsFile.close()
 
     print()
 
