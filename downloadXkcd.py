6f022226e55bb4cc24a2009c6f266738cdc00297
bribrown <bribrown@yahoo.com>
2017-09-08 14:49:48 -0400

    Add files via upload

---
+++ downloadXkcd.py
@@ -0,0 +1,34 @@
+# enter search term
+# pass search term to Google
+# open top 5 links in browser
+
+import webbrowser
+import requests
+import bs4
+import re
+import os
+
+def main():
+
+    comicRequest = requests.get('http://xkcd.com/')
+    comicRequest.raise_for_status()
+
+    soup = bs4.BeautifulSoup(comicRequest.text, 'html.parser')
+
+    comicLink = soup.select('#comic img')
+    comicLink = comicLink[0].get('src')
+
+    print(comicLink)
+
+    comicUrl = 'http:' + comicLink
+    res = requests.get(comicUrl)
+    res.raise_for_status()
+
+    os.chdir('C:\\Users\\bbrownholtz\\Desktop')
+    comicFile = open('1.png', 'wb')
+    for chunk in res.iter_content(100000):
+           comicFile.write(chunk)
+    comicFile.close()
+
+
+if __name__ == "__main__": main()
