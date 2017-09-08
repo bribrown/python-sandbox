1750a91fdd7503eca1908c2b2dfb47aea0fa7909
bribrown <bribrown@yahoo.com>
2017-09-08 15:11:27 -0400

    Add files via upload

--- downloadXkcd.py
+++ downloadXkcd.py
@@ -18,17 +18,21 @@ def main():
     comicLink = soup.select('#comic img')
     comicLink = comicLink[0].get('src')
 
+    prevLink = soup.select('.comicNav a')
+    prevLink = prevLink[1].get('href')
+
     print(comicLink)
+    print(prevLink)
 
     comicUrl = 'http:' + comicLink
     res = requests.get(comicUrl)
     res.raise_for_status()
 
-    os.chdir('C:\\Users\\bbrownholtz\\Desktop')
-    comicFile = open('1.png', 'wb')
-    for chunk in res.iter_content(100000):
-           comicFile.write(chunk)
-    comicFile.close()
+    # os.chdir('C:\\Users\\bbrownholtz\\Desktop')
+    # comicFile = open('1.png', 'wb')
+    # for chunk in res.iter_content(100000):
+    #        comicFile.write(chunk)
+    # comicFile.close()
 
 
 if __name__ == "__main__": main()
