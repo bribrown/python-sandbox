4daad5a2c985558fc2804e14c90ce8dc1523d8b4
bribrown <bribrown@yahoo.com>
2019-10-14 21:42:31 -0400

    Add files via upload

---
+++ weather_rest.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+APPID = os.getenv('APPID')
+
+address='http://api.openweathermap.org/data/2.5/weather'
+
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+parameters = {'id':'524901','APPID':APPID,'units':'imperial'}
+
+response = requests.post(address,headers=headers,params=parameters)
+
+response.raise_for_status()
+
+# Display the JSON results returned
+results = response.json()
+print(json.dumps(results))
+
+print()
+
+key_list=[]
+
+for key in results.keys():
+    key_list.append(key)
+
+print(key_list)
+print(results['main'])
+print(results['main']['temp'])
